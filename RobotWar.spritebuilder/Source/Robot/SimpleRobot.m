696caa57e1f8172357264c915a562d6d7c190ce9
Thanasi Stratigakis <ThanasiStratigakis@Thanasis-MacBook-Pro-Retina.local>
2014-07-03 14:29:40 -0400

    Added comments

--- RobotWar.spritebuilder/Source/Robot/SimpleRobot.m
+++ RobotWar.spritebuilder/Source/Robot/SimpleRobot.m
@@ -56,22 +56,34 @@ typedef NS_ENUM(NSInteger, RobotState) {
     // There are a couple of neat things you could do in this handler
 }
 
+
+// method is called when other robot is scannned
 - (void)scannedRobot:(Robot *)robot atPosition:(CGPoint)position {
+    // if my robot is not shooting then stop all other tasks
     if (_currentRobotState != RobotStateFiring) {
         [self cancelActiveAction];
     }
     
+    // change last known pos variable to the new detected position
     _lastKnownPosition = position;
+    // idk (reset timer)
     _lastKnownPositionTimestamp = self.currentTimestamp;
+    // start shooting
     _currentRobotState = RobotStateFiring;
 }
 
+// method is called when robot hits a wall
+// paramaters are direction of wall and angle of collision
 - (void)hitWall:(RobotWallHitDirection)hitDirection hitAngle:(CGFloat)angle {
+    // if my robot is not turning around, stop doing tasks
     if (_currentRobotState != RobotStateTurnaround) {
         [self cancelActiveAction];
         
+        // save what my robot was just doing (before i stoped it)
         RobotState previousState = _currentRobotState;
+        // set new state to turn around
         _currentRobotState = RobotStateTurnaround;
+        
         
         // always turn to head straight away from the wall
         if (angle >= 0) {
